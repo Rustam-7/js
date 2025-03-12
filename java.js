ac0e5404e0f63bf9c7fb971aaa44d5d398201477
Rustam-7 <joraqulovamahfuza7@gmail.com>
2025-03-12 05:48:14 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+
+let ism = prompt('Ismingizni kiriting')
+console.log(ism);
+
+
+
+let familiya = prompt('familiyangizni kiriting')
+console.log(familiya);
+
+
+let yosh = prompt('yoshingizni kiriting')
+console.log(yosh);
+
+
+let maktab = prompt('maktabingiz raqami')
+console.log(maktab);
+
+let sevimli = prompt('sevimli sportingiz')
+console.log( 'sport' + sevimli);
